e9ab9b8a750d3ef29fc08b0ee799078e518f286c
Nick Hargreaves <hargreaves1234@gmail.com>
2015-07-20 00:57:04 +0300

    send message to user device from dashboard

--- inc/gcm_stuff.php
+++ inc/gcm_stuff.php
@@ -3,7 +3,7 @@
 /*
  * Create Message Content Type
  */
-function messages() {
+function register_message() {
     $labels = array(
         'name'               => _x( 'Messages', 'post type general name' ),
         'singular_name'      => _x( 'Message', 'post type singular name' ),
@@ -21,14 +21,83 @@ function messages() {
     $args = array(
         'labels'        => $labels,
         'description'   => 'Defines message structure',
-        'public'        => false,
+        'public'        => true,
         'menu_position' => 6,
         'supports'      => array( 'title'),
         'has_archive'   => false,
     );
-    register_post_type( 'messages', $args );
+    register_post_type( 'message', $args );
 }
-add_action( 'init', 'messages' );
+add_action( 'init', 'register_message' );
+
+
+//Recipient meta box
+add_action( 'add_meta_boxes', 'recipient_box' );
+function recipient_box() {
+    add_meta_box(
+        'recipient_box',
+        __( 'Choose Message Recipient', 'myplugin_textdomain' ),
+        'recipient_box_content',
+        'message',
+        'side',
+        'high'
+    );
+}
+
+function recipient_box_content( $post ) {
+    wp_nonce_field( plugin_basename( __FILE__ ), 'recipient_box_content_nonce' );
+    $recipient = get_post_meta( get_the_ID(), 'recipient', true);
+    if(empty($recipient)){
+        $recipient = array();
+    }
+
+    //list of users in drop down
+    $blogusers = get_users( 'blog_id=1&orderby=nicename' );
+    print '<select name="recipient">';
+    foreach ( $blogusers as $user ) {
+
+        print '<option value="' . $user->ID . '">' . esc_html($user->user_login) . '</option>';
+
+    }
+    print '</select>';
+    ?>
+    <?php
+}
+
+add_action( 'save_post', 'recipient_box_save' );
+
+function recipient_box_save( $post_id ) {
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return;
+
+    if ( !wp_verify_nonce( $_POST['recipient_box_content_nonce'], plugin_basename( __FILE__ ) ) )
+        return;
+
+    if ( 'page' == $_POST['post_type'] ) {
+        if ( !current_user_can( 'edit_page', $post_id ) )
+            return;
+    } else {
+        if ( !current_user_can( 'edit_post', $post_id ) )
+            return;
+    }
+    $recipient= $_POST['recipient'];
+    update_post_meta( $post_id, 'recipient', $recipient );
+
+    $notified = get_post_meta( $post_id, 'notified' );
+
+    if(empty($notified)){
+        update_post_meta( $post_id, 'notified', "1" );
+
+        $pushMessage = get_the_title($post_id);
+
+        $reg_ids = users_gcm_ids($recipient);
+        $message = array("chat" => $pushMessage);
+        send_push_notification($reg_ids, $message);
+    }
+}
+
+
 /*
  * Send feedback after comment posted
  */
